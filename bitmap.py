f43ecd0f109d75f71a340ddc6e9ea6d3eed39e16
silva <silva@cubee.com>
2017-08-04 00:17:45 +0800

    bitmap tmp

--- bitmap.py
+++ bitmap.py
@@ -7,12 +7,12 @@ class Bitmap():
         self.size = size
         self.data = ''
 
-        div, mod = size/8, size%8
+        self.div, self.mod = size/8, size%8
 
-        if mod > 0:
-            div++
+        if self.mod > 0:
+            self.div++
 
-        for i in range (0, div):
+        for i in range (0, self.div):
             self.data += '\0'
 
     #retrun the bit at index
@@ -21,9 +21,14 @@ class Bitmap():
         mod = prefixLen % 8
         nodeIdDivInt = int(self.data[div])
         # the high bit of nodeid & current prefixLen mod
-        # when prifixlen = 0 ,then prifixlen mod = 0, highbit = 1000000 = 128 & nodeid[div]
-        # the result = 1xxxxxx then >> (7-mod) = the high bit of byte(char)
+        # when prifixlen = 0 ,then prifixlen mod = 0, highbit = 10000000 = 128 & nodeid[div]
+        # the result = 1xxxxxxx then >> (7-mod) = the high bit of byte(char)
         result = ((1 << (7 - mod)) & nodeIdDivInt) >> (7 - mod)
         return result
 
-    def set(self, index, bit):
+    #sets the bit at index `index`.
+    def set(self, index):
+        div, mod = index / 8, index % 8
+
+        shift = 1 << (7-mod)
+        self.data[div] |= shift
